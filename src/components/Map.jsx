61dacbe22b932728ab8bfcc348277041f0518230
Iulian <petruiulian01@gmail.com>
2023-12-22 16:22:52 +0200

    improved navigation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 function Map() {
+
+    const navigate = useNavigate()
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const lat = searchParams.get('lat')
+    const lng = searchParams.get('lng')
+
     return (
-        <div className={styles.mapContainer}>
-            Map
+        <div className={styles.mapContainer} onClick={() => { navigate("form") }}>
+            <h1>Map {lat} {lng}</h1>
+            <button onClick={() => setSearchParams({ lat: 40, lng: -73 })}>Change Position</button>
         </div>
     )
 }
